40d9c2f92bcf6d4333b90c603978ee2c57aed3ec
huni98asd <huninagy98asd@gmail.com>
2023-01-09 21:55:37 +0200

    First commit
    First commit

---
+++ Allamvizsga_01/Assets/Item.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Item 
+{
+
+    public enum ItemType
+    {
+        Right,
+        Left,
+        Jump
+    }
+
+    public ItemType itemType;
+    public int amount;
+
+    public Sprite GetSprite()
+    {
+        switch (itemType)
+        {
+            default:
+            case ItemType.Jump: return ItemAssets.Instance.jumpSprite;
+            case ItemType.Left: return ItemAssets.Instance.goLeftSprite;
+            case ItemType.Right: return ItemAssets.Instance.goRightSprite;
+        }
+    }
+}
